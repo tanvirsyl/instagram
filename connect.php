ad3f93f1509298d52ad8e6e0935b5c67735a911a
Tanvir Hussain <64609749+tanvirsyl@users.noreply.github.com>
2025-05-09 10:57:23 +0100

    db dynamic

--- connect.php
+++ connect.php
@@ -1,12 +1,32 @@
 <?php
 
-	$servername = 'localhost';
-	$user 		= 'root';
-	$pass 		= '';
+	$servername = 'instagramapp.mysql.database.azure.com';
+	$user 		= 'adminapp';
+	$pass 		= 'Adminapp@';
 	$db 		= 'insta_db';
 	$conn 		= mysqli_connect($servername, $user, $pass, $db);
-	
-	if(!$conn){
-		die("").'<br>';
+
+
+	$ssl_ca = _DIR_ . '/ssl/DigiCertGlobalRootCA.crt.pem';
+
+	// Establish connection
+	$conn = mysqli_init();
+	mysqli_options($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+	mysqli_ssl_set(
+		$conn,
+		NULL,  // No client key
+		NULL,  // No client certificate
+		$ssl_ca,
+		NULL,  // No CA path
+		NULL   // No cipher
+	);
+
+	if (!mysqli_real_connect($conn, $hostname, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL)) {
+		die("Connection failed: " . mysqli_connect_error());
 	}
+	
+	// if(!$conn){
+	// 	die("").'<br>';
+	// }
 ?>
+
